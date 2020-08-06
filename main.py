6f37a4f9d2dd875f5c56912780557f67f50ec2c0
amywangyx1022 <awang@lakeviewig.com>
2020-08-06 09:07:47 -0500

    branch1test

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,9 +14,15 @@ def display_ages():
     age = [1, 2, 3]
     for number in age:
         print(number)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     display_ages()
+
+    data = np.random.randint(3, 15, size=(10, 2))
+    df = pd.DataFrame(data, columns=['randomnumbers', 'yrand'])
+    print(df)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
