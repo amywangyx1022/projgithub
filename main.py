60669a4c8764ed5d69d0a05f02fe8a9e989307d1
amywangyx1022 <awang@lakeviewig.com>
2020-08-06 08:37:24 -0500

    testrun

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def display_ages():
+    age = [1, 2, 3]
+    for number in age:
+        print(number)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    display_ages()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
